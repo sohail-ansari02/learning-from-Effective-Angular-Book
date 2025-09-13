23d3af6cd3a96ebb4df749a7dcb2453b919a5bc9
sohail.ansari <sohail.ansari@transneuron.com>
2025-09-13 18:06:44 +0530

    did setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,4 +39,34 @@ export default [
     // Override or add rules here
     rules: {},
   },
+  {
+    files: ['*.ts', '*.tsx'],
+    parserOptions: {
+      project: [
+        'apps/*/*/tsconfig.json',
+        'apps/*/*/tsconfig.*?.json',
+        'libs/*/*/*/tsconfig.*?.json',
+        'libs/workspace-generators-plugin/tsconfig.*?.json',
+      ],
+    },
+    extends: ['plugin:@nx/typescript'],
+    rules: {
+      'deprecation/deprecation': 'warn',
+      'rxjs/no-async-subscribe': 'error',
+      'rxjs/no-create': 'error',
+      'rxjs/no-ignored-error': 'error',
+      'rxjs/no-ignored-notifier': 'error',
+      'rxjs/no-ignored-replay-buffer': 'error',
+      'rxjs/no-ignored-takewhile-value': 'error',
+      'rxjs/no-ignored-subscribe': 'error',
+      'rxjs/no-index': 'error',
+      'rxjs/no-internal': 'error',
+      'rxjs/no-nested-subscribe': 'error',
+      'rxjs/no-redundant-notify': 'error',
+      'rxjs/no-sharereplay': 'error',
+      'rxjs/no-unbound-methods': 'error',
+      'rxjs/no-unsafe-subject-next': 'error',
+      'rxjs/no-unsafe-takeuntil': 'error',
+    },
+  },
 ];
